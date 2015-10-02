cfd4d7c2c9be375ed473287a2927f53244c03d2d
ayush <ayush@ayush.das007@gmail.com>
2015-10-02 15:38:35 +0530

    testing

--- testing/src/testing/Testing.java
+++ testing/src/testing/Testing.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package timtim;
+package testing;
 
 /**
  *
  * @author ayush
  */
-public class Timtim {
+public class Testing {
 
     /**
      * @param args the command line arguments
